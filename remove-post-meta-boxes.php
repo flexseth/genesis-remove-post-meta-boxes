573f560fa1085d8bc149d1c26ce632d5ccecd949
Seth Miller <seth@flexperception.com>
2021-10-29 10:22:43 -0400

    Added SEO meta box snippet

--- remove-post-meta-boxes.php
+++ remove-post-meta-boxes.php
@@ -2,9 +2,14 @@
 // Do not include opening php tag
 // Add to functions.php in your child theme
 
-/* Declutter UI: Remove scripts section from post editor */
+/* Declutter UI: Remove meta boxes from post editor */
 add_action( 'admin_menu' , 'remove_genesis_post_scripts_box' );
 function remove_genesis_post_scripts_box() {
+	// remove Scripts meta box in page and post
 	remove_meta_box( 'genesis_inpost_scripts_box', 'page', 'normal' );
 	remove_meta_box( 'genesis_inpost_scripts_box', 'post', 'normal' );
+	
+	// remove SEO meta box in page and post
+	remove_meta_box( 'genesis_inpost_seo_box', 'page', 'normal' );
+	remove_meta_box( 'genesis_inpost_seo_box', 'post', 'normal' );
 }
